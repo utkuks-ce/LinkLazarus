257015407e1d5765329cb5369cc5f8c317aa48d4
utkuks-ce <utkuks.ce@gmail.com>
2025-06-13 16:21:46 +0300

    debug

--- ryu_path_controller.py
+++ ryu_path_controller.py
@@ -5,6 +5,8 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.topology.api import get_all_switch, get_all_link
 from ryu.topology.event import EventSwitchEnter, EventLinkAdd
+from ryu.lib.packet import packet, ethernet
+
 
 class SimpleTopology(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -12,7 +14,7 @@ class SimpleTopology(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleTopology, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        self.net = {}  # Topology graph (adjacency list)
+        self.net = {}  # Topoloji grafiği, ileride kullanılacak (örneğin yol hesaplama için)
 
     @set_ev_cls(EventSwitchEnter)
     def switch_enter_handler(self, ev):
@@ -23,6 +25,7 @@ class SimpleTopology(app_manager.RyuApp):
     def link_add_handler(self, ev):
         links = get_all_link(self)
         self.logger.info("Current links: %s", [(link.src.dpid, link.dst.dpid) for link in links])
+        # Gelecekte kullanılmak üzere topoloji verisi burada işlenebilir.
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -30,17 +33,24 @@ class SimpleTopology(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
 
+        # Table-miss flow entry
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
-    def add_flow(self, datapath, priority, match, actions):
+    def add_flow(self, datapath, priority, match, actions, idle_timeout=0, hard_timeout=0):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst,
+            idle_timeout=idle_timeout,
+            hard_timeout=hard_timeout)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -50,39 +60,40 @@ class SimpleTopology(app_manager.RyuApp):
         dpid = datapath.id
         in_port = msg.match['in_port']
 
-        pkt = msg.data
         parser = datapath.ofproto_parser
         ofproto = datapath.ofproto
 
-        # Basit MAC öğrenme:
-        eth_src = msg.data[6:12]
-        eth_dst = msg.data[0:6]
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
 
-        # MAC adreslerini hex olarak alın:
-        src_mac = ':'.join(['%02x' % b for b in eth_src])
-        dst_mac = ':'.join(['%02x' % b for b in eth_dst])
+        if eth is None:
+            return  # Ethernet değilse ilgilenme
+
+        src_mac = eth.src
+        dst_mac = eth.dst
 
         self.mac_to_port.setdefault(dpid, {})
-
-        # Kaynağı öğren
         self.mac_to_port[dpid][src_mac] = in_port
         self.logger.info("Switch %s learned MAC %s at port %s", dpid, src_mac, in_port)
 
         if dst_mac in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst_mac]
+            actions = [parser.OFPActionOutput(out_port)]
+
+            match = parser.OFPMatch(in_port=in_port, eth_src=src_mac, eth_dst=dst_mac)
+            self.add_flow(datapath, priority=1, match=match, actions=actions)
+
+            self.logger.info("Flow added on switch %s: %s → %s out port %s", dpid, src_mac, dst_mac, out_port)
         else:
             out_port = ofproto.OFPP_FLOOD
+            actions = [parser.OFPActionOutput(out_port)]
 
-        actions = [parser.OFPActionOutput(out_port)]
+        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
 
-
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = parser.OFPPacketOut(datapath=datapath,
-                                  buffer_id=msg.buffer_id,
-                                  in_port=in_port,
-                                  actions=actions,
-                                  data=data)
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data)
         datapath.send_msg(out)
